2376af890e9274af1421e5c481c30312aa8c7f85
Charles S <charlessoaresdossantos@gmail.com>
2018-11-25 20:19:16 -0200

    webpack module finished

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-/*import * as R from 'ramda';
+import * as R from 'ramda';
+import sum from './util';
 
 const arr1 = [1,2,3,4,5,6];
 const arr2 = [1,2,3,4,7,8];
 const arr3 = R.union(arr1, arr2);
 
-console.log(arr3);*/
-import React from 'react';
-import reactDom from 'react-dom'
+console.log(arr3);
+console.log(sum(8,8));
